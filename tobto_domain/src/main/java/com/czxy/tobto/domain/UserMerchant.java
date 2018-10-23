cdd0983317e7a1d4361878f20e44d80b8c4becfa
赫有佳 <heyoujia@czxy.com>
2018-10-23 17:31:09 +0800

    商品管理,添加一个实体类，UserMerchant，以及修复编辑的bug

---
+++ tobto_domain/src/main/java/com/czxy/tobto/domain/UserMerchant.java
@@ -0,0 +1,49 @@
+package com.czxy.tobto.domain;
+
+import javax.persistence.*;
+
+@Table(name = "user_merchant")
+public class UserMerchant {
+
+    @Column(name = "m_id")
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer mId;
+    @Transient
+    private TMerchant tMerchant;
+    @Column(name = "u_id")
+    private Integer uId;
+    @Transient
+    private User user;
+
+    public Integer getmId() {
+        return mId;
+    }
+
+    public void setmId(Integer mId) {
+        this.mId = mId;
+    }
+
+    public TMerchant gettMerchant() {
+        return tMerchant;
+    }
+
+    public void settMerchant(TMerchant tMerchant) {
+        this.tMerchant = tMerchant;
+    }
+
+    public Integer getuId() {
+        return uId;
+    }
+
+    public void setuId(Integer uId) {
+        this.uId = uId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
